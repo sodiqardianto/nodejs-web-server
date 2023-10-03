cc863d53741b91df058caf6f6b109fce87e0a302
sodiqardianto <sodiqardianto@gmail.com>
2023-10-03 13:46:58 +0700

    Routing Request

--- server.js
+++ server.js
@@ -4,24 +4,34 @@ const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
     response.statusCode = 200;
 
-    const { method } = request;
- 
-    if(method === 'GET') {
-        response.end('<h1>Hello!</h1>');
-    }
- 
-    if(method === 'POST') {
-        let body = [];
+    const { method, url } = request;
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+    if(url === '/') {
+        if(method === 'GET') {
+            response.end('<h1>Ini adalah homepage</h1>');
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+    } else if(url === '/about') {
+        if(method === 'GET') {
+            response.end('<h1>Halo! Ini adalah halaman about</h1>');
+        } else if(method === 'POST') {
+            let body = [];
 
-        request.on('end', () => {
-            body = Buffer.concat(body).toString();
-            const { name } = JSON.parse(body);
-            response.end(`<h1>Hai, ${name}!</h1>`);
-        });
+            request.on('data', (chunk) => {
+                body.push(chunk);
+            });
+
+            request.on('end', () => {
+                body = Buffer.concat(body).toString();
+                const { name } = JSON.parse(body);
+                response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+            });
+        } else {
+            response.end(`<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`);
+        }
+    } else {
+        response.end('<h1>Halaman tidak ditemukan!</h1>');
     }
 };
  
